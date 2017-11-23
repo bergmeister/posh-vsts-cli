6440582fa6c3b66102fae26052c6b73480a8bc8a
Chris <c.bergmeister@gmail.com>
2017-11-23 23:35:17 +0000

    add option tab expansion smoke test

--- vsts-cli-tabcompletion.tests.ps1
+++ vsts-cli-tabcompletion.tests.ps1
@@ -30,5 +30,13 @@ Describe 'vsts-cli-tabcompletion' {
                 $commandCompletion.CompletionMatches.CompletionText | Should Not BeNullOrEmpty # there seems to be a Pester bug because changing the expected result makes the acutal result switch
             }
         }
+
+        It "option expansions expanded" {
+            $commandCompletion = TabExpansion2 -inputScript "vsts build list --t" -cursorColumn 19
+            if (-not $env:APPVEYOR) # tabexpansion2 does not seem to work in appveyor although it uses PS 5.1. Maybe this is related to it not working for PowerShell Core?
+            {
+                $commandCompletion.CompletionMatches.CompletionText | Should Not BeNullOrEmpty # there seems to be a Pester bug because changing the expected result makes the acutal result switch
+            }
+        }
     }
 }
