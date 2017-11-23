114757a6fbe3d8ae17144d4b1286808db63c0023
Chris <c.bergmeister@gmail.com>
2017-11-23 23:30:28 +0000

    add more tab expansion test coverage

--- vsts-cli-tabcompletion.tests.ps1
+++ vsts-cli-tabcompletion.tests.ps1
@@ -1,25 +1,33 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingEmptyCatchBlock", "")]Param()
+
+$poshvstscli_moduleName = 'vsts-cli-tabcompletion'
+Import-Module (Join-Path $PSScriptRoot "$poshvstscli_moduleName.psd1")
+try
+{
+    TabExpansion2 -inputScript "vsts b" -cursorColumn 6 -ErrorAction Ignore # the first time it fails, this is a TODO item
+}
+catch
+{
+
+}
+
 Describe 'vsts-cli-tabcompletion' {
     Context 'Tabexpansion2' {
+        Get-Module $poshvstscli_moduleName | Should Not Be $null
 
         It "vsts b gets expanded correctly to vsts build" {
-            
-
-            $poshvstscli_moduleName = 'vsts-cli-tabcompletion'
-            Import-Module (Join-Path $PSScriptRoot "$poshvstscli_moduleName.psd1")
-            Get-Module $poshvstscli_moduleName | Should Not Be $null
-            try
-            {
-                TabExpansion2 -inputScript "vsts b" -cursorColumn 6 -ErrorAction Ignore # the first time it fails, this is a TODO item
-            }
-            catch
-            {
-
-            }
             $commandCompletion = TabExpansion2 -inputScript "vsts b" -cursorColumn 6
             if (-not $env:APPVEYOR) # tabexpansion2 does not seem to work in appveyor although it uses PS 5.1. Maybe this is related to it not working for PowerShell Core?
             {
                 $commandCompletion.CompletionMatches.CompletionText | Should Be 'build'
+            }
+        }
+        
+        It "vsts build gets expanded" {
+            $commandCompletion = TabExpansion2 -inputScript "vsts build " -cursorColumn 10
+            if (-not $env:APPVEYOR) # tabexpansion2 does not seem to work in appveyor although it uses PS 5.1. Maybe this is related to it not working for PowerShell Core?
+            {
+                $commandCompletion.CompletionMatches.CompletionText | Should Not BeNullOrEmpty # there seems to be a Pester bug because changing the expected result makes the acutal result switch
             }
         }
     }
