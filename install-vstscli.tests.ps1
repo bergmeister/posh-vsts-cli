d04734c3c65f80fd2916f127e039c69b696d6ed9
Chris <c.bergmeister@gmail.com>
2017-11-25 21:42:46 +0000

    import module only in tests to fix code coverage

--- install-vstscli.tests.ps1
+++ install-vstscli.tests.ps1
@@ -1,5 +1,7 @@
 Describe 'Install-VstsCli' {
 
+    Import-Module (Join-Path $PSScriptRoot "posh-vsts-cli.psd1") 
+
     It "installs the vsts cli" {
        Install-VstsCli
        Get-Command vsts | Should Not Be $null
